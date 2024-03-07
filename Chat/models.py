db1eab19b7e62a410b209083bb381f24f8feb66c
McSunRise <70936443+McSunRise@users.noreply.github.com>
2024-03-07 16:02:46 +0300

    Initial commit

---
+++ Chat/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
+
+class User(AbstractUser):
+    __uuid = None
+    def create_user(self, username, password, email):
+        self.username = username
+        self.set_password(password)
+        self.email = email
+        self.uuid = uuid4()
+
+# Create your models here.
