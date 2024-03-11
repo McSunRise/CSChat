386f76de804cc48d846388e04d987d97e68eedd4
McSunRise <70936443+McSunRise@users.noreply.github.com>
2024-03-11 13:26:50 +0300

    UUID as Primary Key

--- Chat/models.py
+++ Chat/models.py
@@ -3,11 +3,9 @@ from django.contrib.auth.models import AbstractUser
 from uuid import uuid4
 
 class User(AbstractUser):
-    __uuid = models.UUIDField()
+    __uuid = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
     def create_user(self, username, password, email):
         self.username = username
         self.set_password(password)
         self.email = email
-        self.uuid = uuid4()
-
 # Create your models here.
