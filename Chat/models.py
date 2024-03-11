83f193601c1a2f30f53d408fcee0912fa459dca1
McSunRise <70936443+McSunRise@users.noreply.github.com>
2024-03-11 13:19:54 +0300

    Development branch

--- Chat/models.py
+++ Chat/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from uuid import uuid4
 
 class User(AbstractUser):
-    __uuid = None
+    __uuid = models.UUIDField()
     def create_user(self, username, password, email):
         self.username = username
         self.set_password(password)
