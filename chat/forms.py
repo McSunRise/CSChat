b0ead2a9bf927cfd77dead2b6a253d6faee8446d
k1 <kiyantsev90@mail.ru>
2024-03-20 04:06:29 +0300

    time 20:31 , pribil satoru gojo. implemented base styles for login page, made common login page html structure. 4:06, vova ya tebya nenaviju (bez negativa)

--- chat/forms.py
+++ chat/forms.py
@@ -1,15 +1,27 @@
-from django import forms
+from django.forms import TextInput, ModelForm, CharField, PasswordInput, Form, EmailInput
 from .models import User
 
 
-class RegForm(forms.ModelForm):
+class RegForm(ModelForm):
 
-    password1 = forms.CharField(widget=forms.PasswordInput, min_length=8, max_length=32)
-    password2 = forms.CharField(widget=forms.PasswordInput, min_length=8, max_length=32)
+    password1 = CharField(
+        widget=PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Password1'}),
+        min_length=8,
+        max_length=32
+    )
+    password2 = CharField(
+        widget=PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Password2'}),
+        min_length=8,
+        max_length=32
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email']
+        widgets = {
+            'username': TextInput(attrs={'class': 'form-input', 'placeholder': 'Username'}),
+            'email': EmailInput(attrs={'class': 'form-input', 'placeholder': 'Email'}),
+        }
 
     def pass_check(self):
         user_password1 = self.cleaned_data['password1']
@@ -18,7 +30,11 @@ class RegForm(forms.ModelForm):
         return user_password1 == user_password2
 
 
-class LogForm(forms.Form):
+class LogForm(Form):
 
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = CharField(
+        widget=TextInput(attrs={'class': 'form-input', 'placeholder': 'Username'}),
+    )
+    password = CharField(
+        widget=PasswordInput(attrs={'class': 'form-input'}),
+    )
